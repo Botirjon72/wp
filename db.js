5033d493cdcaa11ac4982754bc74042b479a061a
Nurmukhamedov Botir <nurmukhamedov_08@icloud.com>
2025-07-20 22:09:58 +0500

    #4

--- db.js
+++ db.js
@@ -2,11 +2,14 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',      // o'zgartiring
-  host: 'localhost',
-  database: 'postgres',      // o'zgartiring
-  password: '1',  // o'zgartiring
+  user: 'asddasf_rz5b_user',
+  host: 'dpg-d1uhiq49c44c73d0ns5g-a.oregon-postgres.render.com',
+  database: 'asddasf_rz5b',
+  password: '9UnLnGV7xgojNSYZqahxi4ZR6GuRjSVu',
   port: 5432,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
